7b4cbd92cac5b299e2caa987325b065aca3e27b4
Matyi <a@a.a>
2023-01-06 15:03:34 +0100

    live db

--- api/db.php
+++ api/db.php
@@ -1,14 +1,18 @@
 <?php
 $username = "c40843admin";
-$servername = "	mysql.rackhost.hu";
+$servername = "mysql.rackhost.hu";
 $password = "zoomdesign";
 $db_name = "c40843syndom";
 
-// Create connection
-$db = new mysqli($servername, $username, $password, $db_name);
+try {
+    // Create connection
+    $db = new mysqli($servername, $username, $password, $db_name);
 
 // Check connection
-if ($db->connect_error) {
-    die("Connection failed: " . $db->connect_error);
+    if ($db->connect_error) {
+        die("Connection failed: " . $db->connect_error);
+    }
+} catch (Exception $e) {
+    echo "errr";
 }
 ?>
