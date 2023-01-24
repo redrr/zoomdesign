2aa145fbc3721116e981444ba25abe0af8a65d90
Redly Matyas <writengo.hu@gmail.com>
2023-01-24 14:04:48 +0100

    update

--- webapp/src/app/frontoffice/common/header/header.component.ts
+++ webapp/src/app/frontoffice/common/header/header.component.ts
@@ -1,5 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
+import {GlobalService} from "../../../globals";
+import {Meta} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-header',
@@ -10,7 +12,7 @@ export class HeaderComponent implements OnInit {
 
   @Input() projectId: any
 
-  constructor(private router: Router) {
+  constructor(private router: Router, private globalService: GlobalService, private meta: Meta) {
     if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
       document.getElementsByName('body').forEach(e => {
         e.classList.add("spec-mobile")
@@ -24,6 +26,11 @@ export class HeaderComponent implements OnInit {
       console.log("Automatic selected language:", userLang)
       localStorage.setItem("lang", (userLang && userLang === "hu-HU") ? "hun" : "en")
     }
+    if (!this.globalService.welcome)
+      this.meta.updateTag({
+        "name": "theme-color",
+        "content": "#ffffff"
+      })
   }
 
   home(): void {
